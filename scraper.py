de54270600519ed02687708cf41c718bf6bf8513
yzqzss <yzqzss@yandex.com>
2023-10-24 01:10:40 +0800

    scraper v2

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import requests as rq
 
 UA = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0"
@@ -7,8 +8,16 @@ national_api = "https://apppc.cnr.cn/national"
 local_list_api = "https://apppc.cnr.cn/local/list"
 local_api = "https://apppc.cnr.cn/local"
 
+sess = rq.session()
+sess.headers["User-Agent"] = UA
 
-def to_disk(path: str, radios):
+scraped_dir = "scraped_data"
+
+def delay_request(r: rq.Response, *args, **kwargs):
+    time.sleep(0.2)
+sess.hooks['response'].append(delay_request)
+
+def to_disk(path: str, radios, radio_dir: str=""):
     # radios = ["data"]["categories"][0]
     with open(path, "w") as f:
         for detail in radios["detail"]:
@@ -19,8 +28,7 @@ def to_disk(path: str, radios):
             logo_url: str = detail["other_info6"]
             stream_url: str = detail["other_info11"][0]["url"]
 
-            f.write("\t"
-            .join([
+            f.write("\t".join([
                 _id if _id else "null",
                 name if name else "null",
                 newsType if newsType else "null",
@@ -29,32 +37,55 @@ def to_disk(path: str, radios):
                 description if description else "null",
             ]) + "\n"
             )
+            if radio_dir:
+                os.makedirs(radio_dir, exist_ok=True)
+                yaml_file = f"{radio_dir}/{name}.yml"
+                logo_file = f"{radio_dir}/{name}.{logo_url.split('.')[-1]}"
+                if os.path.exists(yaml_file):
+                    continue
+                with open(yaml_file, "w") as g:
+                    g.write(
+f"""\
+title: {name}
+description: {description}
+homepage: 
+schedule: 
+logo: {logo_url}
+source-url: {stream_url}
+source-type: hls
+location: CN;
+language: chi
+timezone: Asia/Shanghai
+note: 
+Suggested ID: radio-
+submitted: no
+"""
+                    )
+                with open(logo_file, "wb") as h:
+                    h.write(sess.get(logo_url).content)
 
 
 def main():
-    sess = rq.session()
-    sess.headers["User-Agent"] = UA
     national_json = sess.get(national_api).json()
-    with open("national.json", "w") as f:
+    with open(f"{scraped_dir}/national.json", "w") as f:
         f.write(
             json.dumps(national_json, ensure_ascii=False, separators=(',', ':'), indent=2)
         )
     radios = national_json["data"]["categories"][0]
-    to_disk(path="national.txt", radios=radios)
+    to_disk(path=f"{scraped_dir}/national.txt", radios=radios, radio_dir="National")
 
     local_json_options = sess.get(local_list_api).json()
-    with open("local_options.json", "w") as f:
+    with open(f"{scraped_dir}/local_options.json", "w") as f:
         f.write(json.dumps(local_json_options, ensure_ascii=False, separators=(',', ':'), indent=2))
     for place in local_json_options["liveChannelPlace"]:
         place_id = place["id"]
         place_name = place["name"]
         print(place_id,place_name)
         local_json = sess.post(local_api, json={"id": place_id}).json()
-        with open(f"local_{place_name}.json", "w") as f:
+        with open(f"{scraped_dir}/local_{place_name}.json", "w") as f:
             f.write(json.dumps(local_json, ensure_ascii=False, separators=(',', ':'), indent=2))
         radios = local_json["data"]["categories"][0]
-        to_disk(path=f"local_{place_name}.txt", radios=radios)
+        to_disk(path=f"{scraped_dir}/local_{place_name}.txt", radios=radios, radio_dir=f"Local/{place_name}")
 
 if __name__ == "__main__":
-    os.chdir("scraped_data")
     main()
